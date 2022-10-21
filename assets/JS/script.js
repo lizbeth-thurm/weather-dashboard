f7651a209e1044a3508bc25c1b4da0106f81c3b8
slthurman01 <slthurman01@gmail.com>
2022-10-21 09:53:36 -0500

    Adds call for OpenWeather api to give forecase.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,2 +1,19 @@
-// OpenWeather API Key
-var APIKey = "e61b60c59683fe9b41331ae6e86feeda";
+var APIKey = "e61b60c59683fe9b41331ae6e86feeda"; // OpenWeather API Key
+var cityName; // variable for storing name of city
+var resp; // variable for json response
+var data; // variable for json data
+
+function weatherReport( cityName ) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + APIKey)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      console.log(data);
+    })
+    .catch(function() {
+      // catch any errors
+    });
+  }
+  
+  window.onload = function() {
+    weatherReport( "London" );
+  }
