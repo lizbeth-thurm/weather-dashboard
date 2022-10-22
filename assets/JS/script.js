13976f8437969fb14abca8abf38260d06cdcf21b
slthurman01 <slthurman01@gmail.com>
2022-10-22 10:41:25 -0500

    Adds code to pull and display weather data from openweather api

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,13 +7,23 @@ function weatherReport( cityName ) {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + APIKey)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
-      console.log(data);
+
+      drawWeather(data);
     })
     .catch(function() {
       // catch any errors
     });
   }
-  
+
+function drawWeather( d ) {
+    var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	
+	document.getElementById('description').innerHTML = d.weather[0].description;
+	document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+}
+
   window.onload = function() {
     weatherReport( "London" );
   }
