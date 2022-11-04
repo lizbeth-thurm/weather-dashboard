8d2e9f3d54f62c837b609a6636024cc1782395ca
slthurman01 <slthurman01@gmail.com>
2022-11-04 14:50:04 -0500

    Cleans up city history temporarily-adds start of local storage code.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,19 +4,18 @@ var resp; // variable for json response
 var data; // variable for json data
 var lat; // variable for lattitude of city
 var lon; // variable for longitude of city
+var cityStore; // stores city name
 
 var citySearch = document.getElementById('citySearch');
 var button2 = document.getElementById('button-addon2');
 
 // Logic for getting city name from search box when button is clickedl.
 button2.addEventListener("click", function () {
-  console.log(citySearch.value);
+  localStorage.setItem('cityStore', citySearch.value);
+  console.log(cityStore)
   geoCode(citySearch.value);
   weatherReport(citySearch.value);
 });
-
-// Save data to localStorage
-localStorage.setItem('key', JSON.stringify('value'));
 
 
 //  Weather report for main weather box
@@ -70,29 +69,29 @@ function fiveDay(lat, lon) {
 // Writes five day weather forecast to boxes
 function drawFiveDay(d) {
 
-  document.getElementById('date1').innerHTML = `${d.list[6].dt_txt}`;
-  document.getElementById('temp1').innerHTML = `Temp: ${d.list[6].main.temp}  &deg F`;
-  document.getElementById('wind1').innerHTML = `Wind: ${d.list[6].wind.speed} m/s`;
-  document.getElementById('humidity1').innerHTML = `Humidity: ${d.list[6].main.humidity} %`;
+  document.getElementById('date1').innerHTML = `${d.list[7].dt_txt}`;
+  document.getElementById('temp1').innerHTML = `Temp: ${d.list[7].main.temp}  &deg F`;
+  document.getElementById('wind1').innerHTML = `Wind: ${d.list[7].wind.speed} m/s`;
+  document.getElementById('humidity1').innerHTML = `Humidity: ${d.list[7].main.humidity} %`;
 
-  document.getElementById('date2').innerHTML = `${d.list[14].dt_txt}`;
-  document.getElementById('temp2').innerHTML = `Temp: ${d.list[14].main.temp}  &deg F`;
-  document.getElementById('wind2').innerHTML = `Wind: ${d.list[14].wind.speed} m/s`;
-  document.getElementById('humidity2').innerHTML = `Humidity: ${d.list[14].main.humidity} %`;
+  document.getElementById('date2').innerHTML = `${d.list[15].dt_txt}`;
+  document.getElementById('temp2').innerHTML = `Temp: ${d.list[15].main.temp}  &deg F`;
+  document.getElementById('wind2').innerHTML = `Wind: ${d.list[15].wind.speed} m/s`;
+  document.getElementById('humidity2').innerHTML = `Humidity: ${d.list[15].main.humidity} %`;
 
-  document.getElementById('date3').innerHTML = `${d.list[22].dt_txt}`;
-  document.getElementById('temp3').innerHTML = `Temp: ${d.list[22].main.temp}  &deg F`;
-  document.getElementById('wind3').innerHTML = `Wind: ${d.list[22].wind.speed} m/s`;
-  document.getElementById('humidity3').innerHTML = `Humidity: ${d.list[22].main.humidity} %`;
+  document.getElementById('date3').innerHTML = `${d.list[23].dt_txt}`;
+  document.getElementById('temp3').innerHTML = `Temp: ${d.list[23].main.temp}  &deg F`;
+  document.getElementById('wind3').innerHTML = `Wind: ${d.list[23].wind.speed} m/s`;
+  document.getElementById('humidity3').innerHTML = `Humidity: ${d.list[23].main.humidity} %`;
 
-  document.getElementById('date4').innerHTML = `${d.list[30].dt_txt}`;
-  document.getElementById('temp4').innerHTML = `Temp: ${d.list[30].main.temp}  &deg F`;
-  document.getElementById('wind4').innerHTML = `Wind: ${d.list[30].wind.speed} m/s`;
-  document.getElementById('humidity4').innerHTML = `Humidity: ${d.list[30].main.humidity} %`;
+  document.getElementById('date4').innerHTML = `${d.list[31].dt_txt}`;
+  document.getElementById('temp4').innerHTML = `Temp: ${d.list[31].main.temp}  &deg F`;
+  document.getElementById('wind4').innerHTML = `Wind: ${d.list[31].wind.speed} m/s`;
+  document.getElementById('humidity4').innerHTML = `Humidity: ${d.list[31].main.humidity} %`;
 
-  document.getElementById('date5').innerHTML = `${d.list[38].dt_txt}`;
-  document.getElementById('temp5').innerHTML = `Temp: ${d.list[38].main.temp}  &deg F`;
-  document.getElementById('wind5').innerHTML = `Wind: ${d.list[38].wind.speed} m/s`;
-  document.getElementById('humidity5').innerHTML = `Humidity: ${d.list[38].main.humidity} %`;
+  document.getElementById('date5').innerHTML = `${d.list[39].dt_txt}`;
+  document.getElementById('temp5').innerHTML = `Temp: ${d.list[39].main.temp}  &deg F`;
+  document.getElementById('wind5').innerHTML = `Wind: ${d.list[39].wind.speed} m/s`;
+  document.getElementById('humidity5').innerHTML = `Humidity: ${d.list[39].main.humidity} %`;
 }
 
